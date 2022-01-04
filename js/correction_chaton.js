e7ab55781293d18f09f2d2cae97410f3d1acb09d
Mariam-26 <82433410+Mariam-26@users.noreply.github.com>
2022-01-04 11:31:12 +0100

    chaton

--- js/correction_chaton.js
+++ js/correction_chaton.js
@@ -4,3 +4,15 @@ document.querySelector('#btnForm').addEventListener('click', function() {
     formulaire.innerHTML = 'Votre demande à bien été envoyée. Nous vous répondrons dans les meilleurs delais.';
     formulaire.classList.add('alert', 'alert-success');
 })
+
+// JS pour la tete de chat
+
+let teteDeChat = document.querySelector('#teteDeChat');
+
+teteDeChat.addEventListener('mouseover', function() {
+    this.style.backgroundColor = '#061339';
+
+    teteDeChat.addEventListener('mouseout', function() {
+        this.style.backgroundColor = '#7885A5';
+    })
+})
